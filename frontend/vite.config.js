00b37a1b8b9aa88bddba80ce54c2d5bed50d8913
Sonam Renjen Tamang <sonamrenjen@gmail>
2025-05-23 12:00:39 +0545

    fix vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://employment-management-system-backend.onrender.com',
+        changeOrigin: true,
+        secure: true,
+      },
+    },
+  },
+  preview: {
+    host: true, // Allow external access
+    allowedHosts: [
+      'employment-management-system-frontend.onrender.com',
+      'employment-management-system-backend.onrender.com',
+    ],
+  },
+});
